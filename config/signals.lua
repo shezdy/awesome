7680cf9b743499f6849307db4091772154c9638d
ddmetz <77217897+ddmetz@users.noreply.github.com>
2023-07-03 21:25:46 -0600

    raise on titlebar activate

--- config/signals.lua
+++ config/signals.lua
@@ -27,11 +27,11 @@ client.connect_signal("request::titlebars", function(c)
     -- buttons for the titlebar
     local buttons = gears.table.join(
         awful.button({}, 1, function()
-            c:emit_signal("request::activate", "titlebar", { raise = false })
+            c:emit_signal("request::activate", "titlebar", { raise = true })
             awful.mouse.client.move(c)
         end),
         awful.button({}, 3, function()
-            c:emit_signal("request::activate", "titlebar", { raise = false })
+            c:emit_signal("request::activate", "titlebar", { raise = true })
             awful.mouse.client.resize(c)
         end)
     )
